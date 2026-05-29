43de487477c09c24fc6d7fa50f8f64e1529a4fb7
iamochiai-collab <iamochiai@gmail.com>
2026-05-30 03:06:39 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='money-match-v2';
+const CACHE='money-match-v3';
 const FILES=['./','./index.html','./manifest.webmanifest','./assets/coin_1.png','./assets/coin_5.png','./assets/coin_10.png','./assets/coin_50.png','./assets/coin_100.png','./assets/coin_500.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
